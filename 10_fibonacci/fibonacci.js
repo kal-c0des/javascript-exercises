9bfb3144d5476b358ede77858801dc927461085a
Kalsie Holt <kalsie@student.dev-island.org>
2023-07-17 21:09:13 -1000

    Finish fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) return "OOPS";
+  const fib = [0, 1];
+  for (let i = 1; i < num; i++) {
+    fib.push(fib[i] + fib[i - 1]);
+  }
+  return fib[num];
 };
 
 // Do not edit below this line
