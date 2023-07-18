eac86f4b5cd93a01a56ac964ddec41ad918034c4
Kalsie Holt <kalsie@student.dev-island.org>
2023-07-17 20:32:12 -1000

    Finish palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const checkString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+  return checkString.split("").reverse().join("") == checkString;
 };
 
 // Do not edit below this line
